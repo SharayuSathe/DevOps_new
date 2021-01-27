1df6d12e88ef023928729bd3a7a25afe13f3752b
sharayu <sharayusathe15@gmail.com>
2021-01-27 15:19:27 +0530

    Pom updated with SCM

---
+++ src/main/java/com/example/DevOps/web/welcomeController.java
@@ -0,0 +1,17 @@
+package com.example.DevOps.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class welcomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("Course", "DevOps");
+		return "index";
+
+	}
+
+}
