6666020c9df7e7f33cacc5708623f42708a94c04
sharayu <sharayusathe15@gmail.com>
2021-01-27 19:46:37 +0530

    junut changed

--- src/test/java/com/example/DevOps/web/welcomeControllerTest.java
+++ src/test/java/com/example/DevOps/web/welcomeControllerTest.java
@@ -1,39 +1,5 @@
 package com.example.DevOps.web;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
-@WebAppConfiguration
 public class welcomeControllerTest {
-
-	private MockMvc mockMVC;
-
-	@Autowired
-	private WebApplicationContext context;
-
-	@Before
-	public void setUp() {
-		mockMVC = MockMvcBuilders.webAppContextSetup(context).build();
-	}
-
-	@Test
-	public void welcomeTest() throws Exception {
-
-		/*
-		 * mockMVC.perform(get("/")).andExpect(status().isOk())
-		 * .andExpect(model().attribute("Course", containsString("DevOps")));
-		 */
-
-	}
 
 }
